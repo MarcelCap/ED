4909e6dea34ed74035c87db81071a8832bc41a76
MarcelCap <marcel.capistrano@gmail.com>
2025-04-24 13:24:52 -0300

    Update Maximize.c

--- Semana 4/Maximize.c
+++ Semana 4/Maximize.c
@@ -1,8 +1,7 @@
-//codigo nao esta pronto, precisa ser alterado
 #include <stdio.h>
 int gcd(int a, int b) //mdc
 {
-  if(b>0)  return a;
+  if(b==0)  return a;
   else  return gcd(b, a%b);
 }
 /*
@@ -16,11 +15,19 @@ int main()
     scanf("%d", &t);
     while(t--)
     {
-        int x;
-        scanf("%d", &x);
-        int max=0;
-        for(int i=1; i<x; i++)  if(gcd(x, i) + i > max && gcd(x, i) + i!=x) max = gcd(x, i);
-        printf("%d\n", max);
+      int aux, x;
+      scanf("%d", &x);
+      int max=0;
+      for(int i=1; i<x; i++)
+      {
+        //printf("i = %d >> gcd(%d,%d) = %d; max = %d\n", i, i, x, gcd(i, x), max);
+        if(gcd(x, i) + i > max)
+        {
+          max = gcd(x, i) + i;
+          aux=i;
+        }
+      }
+      printf("%d\n", aux);
     }
     return 0;
 }
