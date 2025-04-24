29d5526ac1b05e09d4e6ac8e2b61ba2f21809076
MarcelCap <marcel.capistrano@gmail.com>
2025-04-24 12:55:21 -0300

    Update Algoritmo_euclides.c

--- Semana 4/Algoritmo_euclides.c
+++ Semana 4/Algoritmo_euclides.c
@@ -2,7 +2,7 @@
 //Sejam a, b inteiros positivos. O Máximo divisor comum de a e b eh dado por:
 int gcd(int a, int b) //mdc
 {
-  if(b>0)  return a;
+  if(b==0)  return a;
   else  return gcd(b, a%b);
 }
 int lcm(int a, int b) //mmc
