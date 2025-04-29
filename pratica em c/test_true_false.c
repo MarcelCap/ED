dfd4d441dda4366bb1887e71ddd7683ea7020321
MarcelCap <marcel.capistrano@gmail.com>
2025-04-29 10:23:48 -0300

    Update test_true_false.c

--- pratica em c/test_true_false.c
+++ pratica em c/test_true_false.c
@@ -7,3 +7,14 @@ int main()
   else	printf("Mentira\n");
   return 0;
 }
+/*
+---OU----
+#include <stdio.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    if(!t)  printf("%d\n", t);
+    return 0;
+}
+*/
